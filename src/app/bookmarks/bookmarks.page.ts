6f47db308f41cd80012864a74ee22f9863b20b33
Robert Baldini <robert.baldini@me.com>
2019-05-08 20:55:21 -0700

    Set up bookmarks list with storage

--- src/app/bookmarks/bookmarks.page.ts
+++ src/app/bookmarks/bookmarks.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { StorageRefs } from '../shared/storage/storage-refs';
+import { Bookmark } from '../core/models/bookmark';
 
 @Component({
   selector: 'app-bookmarks',
@@ -8,11 +10,16 @@ import { Storage } from '@ionic/storage';
 })
 export class BookmarksPage implements OnInit {
 
+  bookmarks: Bookmark[] = [];
+
   constructor(
     private storage: Storage
   ) { }
 
   ngOnInit() {
+    this.storage.get(StorageRefs.FAVORITES).then(favs => {
+      this.bookmarks = favs;
+    });
   }
 
 }
