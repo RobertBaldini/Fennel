72cc69680393ba3693e3304f5b957d0655ea799d
Robert Baldini <robert.baldini@me.com>
2019-05-10 18:55:49 -0700

    initial designs for home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Bookmark } from '../core/models/bookmark';
+import { StorageRefs } from '../shared/storage/storage-refs';
+
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    
+    bookmarksTop3: Bookmark[];
+    bookmarkCount: number;
+
+    constructor(
+        private storage: Storage
+    ) { }
+
+    ngOnInit() {
+        this.storage.get(StorageRefs.FAVORITES)
+                    .then(favs => {
+                        this.bookmarkCount = (favs||[]).length;
+                        this.bookmarksTop3 = (favs||[]).slice(0, 3);
+                    });
+    }
 
 }
