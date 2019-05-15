84a83f7b568e069c0b1e8364ed8dccaef9cfe0b8
Robert Baldini <robert.baldini@me.com>
2019-05-14 22:03:26 -0700

    passing recent searches from home page to search page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -3,6 +3,7 @@ import { SearchService } from './search.service';
 import { RecipeIndex } from '../core/models/recipe-index';
 import { Storage } from '@ionic/storage';
 import { StorageRefs } from '../shared/storage/storage-refs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-search',
@@ -12,21 +13,24 @@ import { StorageRefs } from '../shared/storage/storage-refs';
 export class SearchPage implements OnInit {
 
     searchValue: string;
-    recipeIndex: RecipeIndex[] = [] as RecipeIndex[];
+    recipeReferences: RecipeIndex[] = [] as RecipeIndex[];
 
     constructor(
         private searchService: SearchService,
+        private route: ActivatedRoute,
         private storage: Storage
     ) { }
 
     ngOnInit() {
+        this.searchValue = this.route.snapshot.paramMap.get('searchValue');
+        
+        this.goSearch();
     }
 
     goSearch() {
         if (!this.searchValue || this.searchValue.length < 1)
             return;
 
-        // console.log('goSearch: ' + this.searchValue);
         this.storage.get(StorageRefs.SEARCHES).then(history => {
             history = history || [];
             history.unshift(this.searchValue);
@@ -36,7 +40,7 @@ export class SearchPage implements OnInit {
         });
 
         this.searchService.searchQuery(this.searchValue).subscribe(searchResults => {
-            this.recipeIndex = searchResults.results;
+            this.recipeReferences = searchResults.results;
         });
     }
 
