a45e796a2c75ecff99cb75fbb538ca81f28c19e9
Robert Baldini <robert.baldini@me.com>
2019-05-14 20:34:18 -0700

    added some UX adjustments that depend on how many items are in history

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { StorageRefs } from '../shared/storage/storage-refs';
 
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -11,26 +12,45 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage implements OnInit {
 
-    historyTop4: string[];
-    bookmarksTop3: Bookmark[];
+    historyTop4 = [] as string[];
+    bookmarksTop3 = [] as Bookmark[];
     bookmarkCount: number;
 
     constructor(
-        private storage: Storage
+        private storage: Storage,
+        private router: Router
     ) { }
 
     ngOnInit() {
+    }
+
+    // ngOnInit only fires on a fresh page load. 
+    // This ionic lifecycle hook gets called every time.
+    ionViewDidEnter() {
+        this.refreshBookmarks();
+        this.refreshSearches();
+    }
+
+    refreshBookmarks() {
         this.storage.get(StorageRefs.FAVORITES)
             .then(favs => {
                 this.bookmarkCount = (favs || []).length;
                 this.bookmarksTop3 = (favs || []).slice(0, 3);
-            });
 
+                // redirect to search if there's nothing special to display on home
+                if (this.bookmarksTop3.length == 0) {
+                    this.router.navigateByUrl('/search');
+                    return;
+                }
+            });
+    }
+
+    refreshSearches() {
         this.storage.get(StorageRefs.SEARCHES).then(history => {
             history = history || [];
             let top4 = history.slice(0, 4);
             this.historyTop4 = top4;
-        })
+        });
     }
 
 }
