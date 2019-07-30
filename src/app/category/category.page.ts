b8eccd30009d62f43c66d56a0d2e9c4e59316545
Robert Baldini <robert.baldini@me.com>
2019-07-29 18:27:51 -0700

    implemented infinite scroll for category results

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
-import { StorageRefs } from '../shared/storage/storage-refs';
 
+import { StorageRefs } from '../shared/storage/storage-refs';
 import { CategoryHistory } from '../core/models/category-history';
 import { RecipeReference } from '../core/models/recipe-reference';
-import { SearchResults } from '../core/models/search-results';
-
 import { SearchService } from '../search/search.service';
 
 @Component({
     selector: 'app-category',
     templateUrl: './category.page.html',
     styleUrls: ['./category.page.scss'],
+    providers: [SearchService]
 })
 export class CategoryPage implements OnInit {
+    @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
     allCategoriesHistory = [] as CategoryHistory[];
     categoryHistory = new CategoryHistory();
@@ -31,46 +32,58 @@ export class CategoryPage implements OnInit {
         this.categoryType = this.route.snapshot.data.searchType;
     }
 
-    ionViewDidEnter() {
+    async ionViewDidEnter() {
         if (!this.categoryType) {
             console.log('category type not provided');
             return;
         }
-        this.getNumberAlreadySeen().then(() => this.goSearch());
+        await this.goSearch();
     }
 
-    getNumberAlreadySeen() {
-        // skip however many of this category that
-        // the user has already scrolled through
-        return this.storage.get(StorageRefs.CATEGORIES).then((categories: CategoryHistory[]) => {
-            this.allCategoriesHistory = categories || [];
-            let currentCategory = this.allCategoriesHistory.find(c => c.categoryType == this.categoryType);
-            if (currentCategory) {
-                this.categoryHistory = currentCategory;
-                return;
-            }
-            this.categoryHistory = new CategoryHistory();
-            this.categoryHistory.categoryType = this.categoryType;
-            this.allCategoriesHistory.push(this.categoryHistory);
-        });
-    }
+    async goSearch() {
+        await this.getNumberAlreadySeen();
 
-    goSearch() {
         this.searchService
             .setType(this.categoryType)
             .setOffset(this.categoryHistory.maxNumberSeen);
 
-        this.searchService.searchQuery(null).subscribe((searchResults: SearchResults) => {
-            this.searchResults = searchResults.results;
-            let incrementAmount = this.searchResults.length;
-            this.incrementNumberSeen(incrementAmount);
-        });
+        let searchResult = await this.searchService.searchQuery(null).toPromise();
+        if (!searchResult || !searchResult.results)
+            return;
+
+        searchResult.results.forEach(result => this.searchResults.push(result));
+
+        let incrementAmount = this.searchResults.length;
+        await this.incrementNumberSeen(incrementAmount);
     }
 
-    incrementNumberSeen(incrementAmount: number) {
-        this.categoryHistory.maxNumberSeen += incrementAmount;
+    async getNumberAlreadySeen() {
+        // skip however many of this category that
+        // the user has already scrolled through
+        var categories = await this.storage.get(StorageRefs.CATEGORIES);
+        this.allCategoriesHistory = categories || [];
+        let currentCategory = this.allCategoriesHistory.find(c => c.categoryType == this.categoryType);
+        if (currentCategory) {
+            this.categoryHistory = currentCategory;
+            return;
+        }
+        this.categoryHistory = new CategoryHistory();
+        this.categoryHistory.categoryType = this.categoryType;
+        this.allCategoriesHistory.push(this.categoryHistory);
+    }
+
+    async incrementNumberSeen(incrementAmount: number) {
+        if (this.categoryHistory.maxNumberSeen >= 890) // offset limit is 900 max from the 3rd party api
+            this.categoryHistory.maxNumberSeen = 0;
+        else
+            this.categoryHistory.maxNumberSeen += incrementAmount;
         //let currentCategory = this.allCategoriesHistory.find(c => c.categoryType == this.categoryType);
-        this.storage.set(StorageRefs.CATEGORIES, this.allCategoriesHistory);
+        await this.storage.set(StorageRefs.CATEGORIES, this.allCategoriesHistory);
+    }
+
+    async loadMore(event) {
+        await this.goSearch();
+        event.target.complete();
     }
 
 }
